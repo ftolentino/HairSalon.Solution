2e484382e75943f79bb715b4d764ea48ab2d6de7
ftolentino <filmer.tolentino@gmail.com>
2022-07-29 11:27:47 -0700

    Update properties

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,12 +1,12 @@
 namespace HairSalon.Models
 {
-  public class Restaurant
+  public class Client
   {
-    public int RestaurantId { get; set; }
+    public int ClientId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Location { get; set; }
-    public int CuisineId { get; set; }
-    public virtual Cuisine Cuisine { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
